564adde7071d7e369a1369856e7556b41d5355f5
Adrian Macaraeg <10acpm@gmail.com>
2024-11-07 22:00:19 -0800

    backtrack

--- 2025/01-06/two-sum.js
+++ 2025/01-06/two-sum.js
@@ -35,4 +35,9 @@ twoSum(nums, target){
             for (let j = i + 1; j < nums.length; j++) {
                 if (nums[i] + nums[j] === target) {
                     return [i, j]; 
+                }
+            }
+        }
+        return [];
+    }
 }
