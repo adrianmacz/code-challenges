5f0bcd81d038536b0a3db25dde9c1428d57d6166
Adrian Macaraeg <10acpm@gmail.com>
2025-04-05 22:02:12 -0700

    added a comment

--- 2025/04-05/even-or-odd.js
+++ 2025/04-05/even-or-odd.js
@@ -4,3 +4,6 @@ function evenOrOdd(number) {
   }
   return "Odd";
 }
+
+//modulus is the remainder operator returns the remainder left over when one operand is divided by a second operand.
+// It always takes the sign of the dividend
